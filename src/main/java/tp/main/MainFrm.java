09ec6a7d2722c015fa008a820deb4deb0dcc0d85
Tomasz <piotrowski.kat@gmail.com>
2019-02-12 13:39:37 +0100

    Update

--- src/main/java/tp/main/MainFrm.java
+++ src/main/java/tp/main/MainFrm.java
@@ -1,8 +1,10 @@
 package tp.main;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.EventQueue;
 
+import javax.swing.Icon;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
@@ -13,12 +15,22 @@ import javax.swing.JSplitPane;
 import javax.swing.JTable;
 import javax.swing.JToolBar;
 import javax.swing.border.TitledBorder;
+import javax.swing.text.IconView;
 
 import com.google.gson.GsonBuilder;
 
 import net.miginfocom.swing.MigLayout;
+import tp.panels.ContactsListPanel;
+
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.io.File;
+import java.util.HashMap;
 import java.awt.event.ActionEvent;
 import javax.swing.JTabbedPane;
 
@@ -50,13 +62,15 @@ public class MainFrm {
 
 	}
 
-	
-	
+	HashMap<String, Component> openedDataBasesHashMap = new HashMap<String, Component>();
+
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
 		frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		frame.addWindowListener(windowListener());
 		frame.setBounds(100, 100, 620, 469);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -67,7 +81,7 @@ public class MainFrm {
 		menuBar.add(mnFile);
 
 		JMenuItem mntmNew = new JMenuItem("New file");
-		
+
 		mnFile.add(mntmNew);
 
 		JMenuItem mntmOpen = new JMenuItem("Open file");
@@ -81,20 +95,60 @@ public class MainFrm {
 
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		frame.getContentPane().add(tabbedPane, BorderLayout.CENTER);
-		
+
 		mntmNew.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if (e.getSource() == mntmNew) {
 					JFileChooser fileChooser = new JFileChooser();
 					int retVal = fileChooser.showOpenDialog(frame);
-					
-					
-					
-				}
+					if (retVal == JFileChooser.APPROVE_OPTION) {
 
+						addDbEditorTab(tabbedPane, fileChooser);
+					}
+				}
 			}
 		});
-		
+	}
+
+	
+	
+	private void addDbEditorTab(JTabbedPane tabbedPane, JFileChooser fileChooser) {
+		File selectedFile = fileChooser.getSelectedFile();
+
+		String dbFilePath = selectedFile.getAbsolutePath();
+
+		if (!openedDataBasesHashMap.containsKey(dbFilePath)) {
+
+			var dbEditor = new ContactsListPanel();
+
+			dbEditor.setSource(dbFilePath, !selectedFile.exists());
+
+			openedDataBasesHashMap.put(dbFilePath, dbEditor);
+
+			tabbedPane.addTab(dbFilePath, dbEditor);
+		}
+
+	}
+
+	private WindowAdapter windowListener() {
+		return new WindowAdapter() {
+
+			@Override
+			public void windowClosing(WindowEvent e) {
+				if (JOptionPane.showConfirmDialog(frame, 
+			            "Are you sure you want to close this window?", "Close Window?", 
+			            JOptionPane.YES_NO_OPTION,
+			            JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION){
+			            System.exit(0);
+			        }
+			}
+
+			@Override
+			public void windowClosed(WindowEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+		};
 	}
 
 }
--- src/main/java/tp/panels/ContactsListPanel.java
+++ src/main/java/tp/panels/ContactsListPanel.java
@@ -3,13 +3,18 @@ package tp.panels;
 import javax.swing.JPanel;
 import javax.swing.JSplitPane;
 import javax.swing.border.TitledBorder;
+
 import net.miginfocom.swing.MigLayout;
 import tp.interfaces.ContactBookService;
+import tp.model.Person;
+import tp.model.PersonsTableModel;
 import tp.services.ContactBookServiceFactory;
 
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import java.awt.GridLayout;
+import java.util.ArrayList;
+
 import javax.swing.JToolBar;
 import javax.swing.JTable;
 
@@ -18,45 +23,62 @@ public class ContactsListPanel extends JPanel {
 
 	String source;
 	ContactBookService service;
-	
+
 	/**
 	 * Create the panel.
 	 */
 	public ContactsListPanel() {
 		setLayout(new GridLayout(1, 0, 0, 0));
-		
+
 		JSplitPane splitPane = new JSplitPane();
 		splitPane.setResizeWeight(0.65);
 		add(splitPane);
-		
+
 		JPanel panel = new JPanel();
 		panel.setBorder(new TitledBorder(null, "Contacts", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		splitPane.setLeftComponent(panel);
 		panel.setLayout(new MigLayout("", "[grow]", "[][grow]"));
-		
+
 		JToolBar toolBar = new JToolBar();
 		panel.add(toolBar, "cell 0 0");
-		
+
 		table = new JTable();
 		panel.add(table, "cell 0 1,grow");
-		
+
 		JPanel panel_1 = new JPanel();
-		panel_1.setBorder(new TitledBorder(null, "Contact details", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel_1.setBorder(
+				new TitledBorder(null, "Contact details", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		splitPane.setRightComponent(panel_1);
 	}
-	
-	public void setSource(String source)
-	{
+
+	public void setSource(String source, Boolean isNew) {
 		this.source = source;
-		
+
 		service = ContactBookServiceFactory.factory(source);
-		
+
 		try {
-			service.Initialize();
-			
+			if (!isNew) {
+				service.Initialize();
+				var list = service.getPersons();
+
+				FillJTable(table, list);
+			} else {
+				var list = service.getPersons();
+
+				FillJTable(table, list);
+			}
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+
+	protected void FillJTable(JTable table, ArrayList<Person> persons) {
+		var model = new PersonsTableModel(persons);
+
+		table.setModel(model);
+
+	}
+
 }
